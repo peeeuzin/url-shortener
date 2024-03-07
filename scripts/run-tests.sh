e3b11f453cadd0382af1e4fa4895dd7b82a7cd3e
peeeuzin <canaldopedrinho233@gmail.com>
2024-03-07 10:04:17 -0300

    ci: create .env.local on ci

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,6 +4,25 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+touch_env=0
+
+while (($#)); do
+    [[ $1 = -- ]] && {
+        shift
+        break
+    }
+    [[ $1 = -?* ]] || break
+    case $1 in
+    --touch-env) touch_env=1 ;;
+    -*) invalid_option "$1" ;;
+    esac
+    shift
+done
+
+if [[ $touch_env -eq 1 ]]; then
+    touch .env.local
+fi
+
 docker compose up postgres --wait -d
 
 cargo test
